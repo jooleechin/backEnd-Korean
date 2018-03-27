79b91a622ca3013a3b95ce01e8c882d7eb8fa95d
Joolee Chin <jooleechin@gmail.com>
2018-03-27 09:55:43 -0700

    made adjustments to the ERD

--- db/seeds/00_drop.js
+++ db/seeds/00_drop.js
@@ -3,4 +3,5 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(() => knex('learn').del())
     .then(() => knex('questions').del())
+    .then(() => knex('users_questions').del())
 }
---
+++ db/seeds/04_users_questions.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users_questions').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users_questions').insert([
+        {id: 1, user_id: '1', question_id: '1', isCorrect: true},
+        {id: 2, user_id: '1', question_id: '2', isCorrect: false},
+        {id: 3, user_id: '1', question_id: '3', isCorrect: true},
+        {id: 4, user_id: '1', question_id: '4', isCorrect: false},
+        {id: 5, user_id: '1', question_id: '5', isCorrect: true}
+      ]);
+    })
+    .then(() => {
+      return knex.raw(
+        `SELECT setval('users_questions_id_seq', (SELECT MAX(id) FROM users_questions))`
+      )
+    })
+};
