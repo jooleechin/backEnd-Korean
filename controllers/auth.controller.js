522383b3ce8d4deebe693a2654a021b5adbeba4e
Joolee Chin <jooleechin@gmail.com>
2018-03-30 11:42:56 -0700

    added validation for signup/signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const authModel = require('../models/auth.model')
 
 let login = (req, res, next) => {
   const { email, password } = req.body
+  // if (!email || !password) return next({status: 400, {message: 'Email and password are required!'}})
   authModel.comparePass(email, password)
     .then(matches => {
       res.json({ matches })
@@ -11,11 +12,14 @@ let login = (req, res, next) => {
 
 let signup = (req, res, next) => {
   let { fName, email, password } = req.body
-  authModel.signup(fName, email, password)
-    .then (results => {
-      res.json(results)
-    })
-    .catch(err => next(err))
+  // if (!fName || !email || !password) return next({status: 400, {message: 'All fields need to be filled out'}})
+  if (fName && email && password) {
+    authModel.signup(fName, email, password)
+      .then (results => {
+        res.json(results)
+      })
+      .catch(err => next(err))
+  }
 }
 
 module.exports = {
